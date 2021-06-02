3b2026295cff32d8bdc2ba7237e76e6dab7656f7
Sisihlia Yuniyarti <sisihlia.yuniyarti@gmail.com>
2021-06-02 21:46:24 +0000

    complete run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=sisihliay/projectml
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+minikube kubectl run projectml -- --image=sisihliay/projectml:v1 --port=80
 
 # Step 3:
 # List kubernetes pods
+minikube kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+minikube kubectl port-forward projectml 8000:80
